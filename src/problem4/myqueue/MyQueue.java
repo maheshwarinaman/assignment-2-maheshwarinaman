98be8fcf0fb38efb82d2ff8e4035a047ccb1f58b
maheshwarinaman <49190119+maheshwarinaman@users.noreply.github.com>
2020-03-27 23:57:55 +0530

    Update MyQueue.java

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,4 +8,23 @@ package problem4.myqueue;
 // to create queue to store pre - order successor
 
 public class MyQueue {
+  MyLinkedList linkedList = new MyLinkedList();
+    public int peek(int data){
+        if(linkedList.search(data)==null){
+            return -1;
+        }
+        return linkedList.search(data).getData();
+    }
+
+    public void enqueue(int data){
+        linkedList.addLast(data);
+    }
+
+    public void dequeue(){
+        linkedList.deleteFirst();
+    }
+
+    public void show(){
+        linkedList.display();
+    }
 }
